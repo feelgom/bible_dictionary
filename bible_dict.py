d107d1d9233bbc3d6a48ca98a506370f672f1f3c
feelgom <ysji@islab.snu.ac.kr>
2022-05-02 02:17:10 +0900

    V0.2
    Using only one txt file

--- bible_dict.py
+++ bible_dict.py
@@ -23,17 +23,11 @@ def main(input_list):
         jang = str(input_list[1])
         jul = str(input_list[2])
         
-        
-    # if book_name in name_long2short:
-    #     book_name = name_long2short[book_name]
-    
-    assert book_name in book_name_dict, book_name+" - 권 이름이 잘 못 됐습니다"
-    file_name = book_name_dict[book_name]
     find_key = book_name+jang+":"+jul
 
     count = 0
     error = True
-    with open(file_name, encoding='cp949') as f:
+    with open('bible.txt', encoding='cp949') as f:
         book_string = f.read()
         book_split = book_string.split('\n')
         for lines in book_split:
@@ -49,21 +43,7 @@ def main(input_list):
         print("입력 값을 다시 확인해주세요 : ", find_key)
 
 
-
 if __name__ =="__main__":
-    book_name_dict = {}
-    name_long2short = {}
-    file_list = glob.glob('text/*.txt')
-    for file_name in file_list:
-        key1 = os.path.basename(file_name).split('.')[0][4:]
-        line = open(file_name, encoding='cp949').readline()
-        key2 = line.split('1')[0]
-
-        book_name_dict[key1] = file_name
-        book_name_dict[key2] = file_name
-        name_long2short[key1] = key2
-
-
     if len(sys.argv) == 1:
         while 1:
             input_list = input('\n검색 내용을 입력하세요: ').split()
